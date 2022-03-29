596f24f9566ee90de31859c33111886016b17dc9
arianamesquita <arianamesquita85@gmail.com>
2022-03-29 10:56:39 -0300

    inclusão pasta do curso Introducao a Criacao de Websites com HTML5 e CSS3

---
+++ Introducao a Criacao de Websites com HTML5 e CSS3/Exercício1.php
@@ -0,0 +1,49 @@
+<html>
+
+	<head>
+		<meta charset="utf-8">
+		<title> Jiddu Krishnamurti </title>
+		<link rel="stylesheet" href="style.css">
+	</head>
+	
+	<body>
+	
+		<header>
+			<img src="krishnamurti.jpg" alt="Foto de Jiddu Krishnamurti retirada da internet." class="photo">
+			<h1 id="title"> "Jiddu Krishnamurti" </h1>
+		</header>
+		
+		<section>
+			<header>
+				<h2 class="subtitle">"Posts"</h2>
+			</header>
+			<article class="post">
+				<header>
+				<h3 class="post_title">"Post #1"</h3>
+				<img src="download.jpg" alt="Foto de Jiddu Krishnamurti retirada da internet." class="post_image">
+				</header>
+				<p class="post_content">
+					Estar consciente - <a href="https://textosk.wordpress.com/trechos-curtos/estar-consciente/" target="_blank">Jiddu Krishnamurti</a> <br>
+					Interrogador: Gostaria de saber o que quer dizer com estar consciente porque disse muitas vezes que o seu ensinamento é na realidade sobre o estar consciente. Tentei compreender ouvindo as suas palestras e lendo os seus livros, mas não pareço ir muito longe. Sei que não é uma prática, e compreendo porque é que tão enfaticamente repudia qualquer tipo de prática, exercício, sistema, disciplina ou rotina. Vejo a importância disso, porque caso contrário o estar consciente torna-se mecânico, e no final a mente tornou-se embotada e estúpida. Gostaria, se me permite, de explorar consigo até ao fim esta questão do que significa estar consciente. O senhor parece dar a esta palavra um significado extra, mais profundo, e, contudo parece-me que estamos conscientes do que acontece a toda a hora. Quando estou irado sei-o, quando estou triste sei-o e quando estou feliz sei-o.<br>
+					<a href="mailto:ariana@gmail.com">Krishnamurti</a>: Pergunto-me se realmente estamos conscientes da ira, da tristeza, da felicidade. Ou estamos conscientes destas coisas somente quando acabam? Vamos começar como se não soubéssemos absolutamente nada acerca disto e começássemos do princípio. Não façamos quaisquer afirmações, dogmáticas ou sutis, mas exploremos esta questão que, se realmente a investigássemos muito profundamente, revelaria um estado extraordinário com o qual a mente provavelmente nunca esteve em contato, uma dimensão não tocada pela consciência superficial. Comecemos do superficial e avancemos. Vemos com os nossos olhos, percebemos com os nossos sentidos as coisas à nossa volta – a cor da flor, o colibri sob a flor, a luz deste sol californiano, os milhares de sons de diferentes qualidades e sutilezas, a profundidade e a altitude, a sombra da árvore e a própria árvore. Do mesmo modo sentimos os nossos próprios corpos que são os instrumentos destes diferentes tipos de percepções superficiais, sensoriais. Se estas percepções permanecessem ao nível superficial não haveria nenhuma confusão. Aquela flor, aquele amor-perfeito, aquela rosa, estão ali, e isso é tudo. Não há preferência, não há comparação, não há o gostar ou o não gostar, somente a coisa perante nós sem qualquer envolvimento psicológico. Está clara toda esta percepção ou consciência superficial sensorial? Ela pode ser expandida até às estrelas, até a profundeza dos mares, e até às derradeiras fronteiras da observação científica, usando todos os instrumentos da tecnologia moderna.
+				</p>
+			</article>
+		</section>
+		
+		<footer>
+			<ul class="contacts_list">
+				<li>
+					<a href="mailto:ariana@gmail.com">ariana@gmail.com</a>
+				</li>
+				<li>
+					<a href="https://www.linkedin.com/in/ariana-gon%C3%A7alves-8b6a553b/" target="_blank">LinkedIn</a>
+				</li>
+				<li>
+					<a href="https://github.com/arianamesquita" target="_blank">Github</a>
+				</li>
+			</ul>
+		</footer>
+		
+	</body>
+	
+</html>
